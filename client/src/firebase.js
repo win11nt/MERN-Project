a3acd9578336669fa8ebbf8311741e33d6a64c37
Minh <achillesback0511@yahoo.com>
2025-12-05 19:51:54 +0700

    Fix Firebase v9 imports for Render build

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,22 @@
-import * as firebase from "firebase";
+// firebase.js (Firebase v9 compatible)
+
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
-const firebaseConfig ={ 
-  apiKey:"AIzaSyBs0Tzjg_10epR0uxE7yYS5FEGXobkKl1w", 
-  authDomain:"ecommerce-cc3f3.firebaseapp.com", 
-  projectId:"ecommerce-cc3f3", 
-  storageBucket:"ecommerce-cc3f3.appspot.com", 
-  messagingSenderId:"212186369843", 
-  appId:"1:212186369843:web:3da7f302727962c1cc3846", 
-  measurementId:"G-KV2C75GEYC" 
+const firebaseConfig = {
+  apiKey: "AIzaSyBs0Tzjg_10epR0uxE7yYS5FEGXobkKl1w",
+  authDomain: "ecommerce-cc3f3.firebaseapp.com",
+  projectId: "ecommerce-cc3f3",
+  storageBucket: "ecommerce-cc3f3.appspot.com",
+  messagingSenderId: "212186369843",
+  appId: "1:212186369843:web:3da7f302727962c1cc3846",
+  measurementId: "G-KV2C75GEYC",
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-// export
-export const auth = firebase.auth();
-export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export auth + Google provider
+export const auth = getAuth(app);
+export const googleAuthProvider = new GoogleAuthProvider();
